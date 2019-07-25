ecb7ddd6ea70eca5dbd128e25eea844dbda3170f
LanFly <1260022720@qq.com>
2019-07-25 15:59:26 +0800

    add travis

---
+++ scripts/install-idf-v3.2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd ~/
+
+wget https://github.com/espressif/esp-idf/releases/download/v3.2/esp-idf-v3.2.zip
+tar -zxf esp-idf-v3.2.zip
---
+++ scripts/install-xtensa.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd ~/
+
+wget https://dl.espressif.com/dl/xtensa-esp32-elf-gcc8_2_0-esp32-2019r1-linux-amd64.tar.gz
+tar -zxf xtensa-esp32-elf-gcc8_2_0-esp32-2019r1-linux-amd64.tar.gz
