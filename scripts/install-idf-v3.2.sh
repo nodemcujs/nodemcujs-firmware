1eb650c7d9fa734de5745784ab853df7fcd04a95
LanFly <1260022720@qq.com>
2019-07-25 16:57:09 +0800

    debug ci

--- scripts/install-idf-v3.2.sh
+++ scripts/install-idf-v3.2.sh
@@ -3,4 +3,5 @@
 cd ~/
 
 wget https://github.com/espressif/esp-idf/releases/download/v3.2/esp-idf-v3.2.zip
-unzip esp-idf-v3.2.zip
+unzip -q esp-idf-v3.2.zip
+python -m pip install --user -r esp-idf-v3.2/requirements.txt
